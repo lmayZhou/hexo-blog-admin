4b2442c325764a2cdce73c97a2912ba9e3384fb0
lmay <lmay@lmaye.com>
2021-01-21 12:19:31 +0800

    -- feat: OSS文件上传.

--- core/utils/oss_upload.py
+++ core/utils/oss_upload.py
@@ -14,13 +14,15 @@ import uuid
 from pathlib import Path
 from minio import Minio
 
-
+# access_key: MinIo帐号
+# secret_key: MinIo密码
 minio_storage = Minio("192.168.30.180:9000", access_key='admin', secret_key='YouGuess', secure=False)
 images = sys.argv[1:]
 for image in images:
     print("File Uploading ...")
     suffix = Path(image).suffix
     file_name = str(uuid.uuid4()) + suffix
-    minio_storage.fput_object("hexo-blog", file_name, image, content_type="image/png",
-                              part_size=10485760)
-    print("http://192.168.30.180/files/hexo-blog/{}".format(file_name))
+    # 存储桶名称
+    bucket_name = "hexo-blog"
+    minio_storage.fput_object(bucket_name, file_name, image, content_type="image/png", part_size=10485760)
+    print("http://192.168.30.180/files/{}/{}".format(bucket_name, file_name))
